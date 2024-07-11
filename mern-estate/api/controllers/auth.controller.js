a7aff17ff5577656cfd3e58a62f525e21adb3e7c
Ricardo Laranjeira <ricardo.laranjeira.74@gmail.com>
2024-07-11 13:42:43 -0400

    sign in controller

--- mern-estate/api/controllers/auth.controller.js
+++ mern-estate/api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js"
+
+const hours = 60*60*1000
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
@@ -15,5 +18,25 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
+}
 
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+
+  try {
+    
+    const validUser = await User.findOne({ email })
+    if (!validUser) return next(errorHandler(404,"User not found!"))
+    
+    const validPassword = bcryptjs.compare(password, validUser.password)
+    if (!validPassword) return next(errorHandler(401,"Invalid credentials!"))
+    
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const { password: pass, ...userData} = validUser._doc
+
+    res.cookie("access_token", token, {httpOnly:true, expires: new Date(Date.now()+2*hours)}).status(200).json(userData)
+
+  } catch (error) {
+    next(error)
+  }
 }
