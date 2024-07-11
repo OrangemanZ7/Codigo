3fd5447d669a20e83293d533294e0265a08a2002
Ricardo Laranjeira <ricardo.laranjeira.74@gmail.com>
2024-07-10 21:42:19 -0400

    sinup auth

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -3,6 +3,7 @@ import express from "express"
 import mongoose from "mongoose"
 
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config()
 
@@ -24,4 +25,7 @@ app.listen(PORT, () =>{
   console.log(`Server running on port: ${PORT}`)
 })
 
+app.use(express.json())
+
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
