2ea40bb296a99c67330654a45d2ab9b1ddf94e72
Ricardo Laranjeira <ricardo.laranjeira.74@gmail.com>
2024-07-10 20:03:41 -0400

    connect to mongo

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -1,6 +1,20 @@
+import dotenv from "dotenv"
 import express from "express"
+import mongoose from "mongoose"
 
-const PORT = process.env.PORT || 3000
+dotenv.config()
+
+const PORT = process.env.PORT
+const DB_USERNAME=process.env.DB_USERNAME
+const DB_PASSWORD=process.env.DB_PASSWORD
+const DB_URL=process.env.DB_URL
+const DB_APPNAME=process.env.DB_APPNAME
+
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}/?retryWrites=true&w=majority&appName=${DB_APPNAME}`).then(() => {
+  console.log(`Connected to MongoDB/${DB_APPNAME}...`)
+}).catch((e) => {
+  console.log(e)
+})
 
 const app = express()
 
